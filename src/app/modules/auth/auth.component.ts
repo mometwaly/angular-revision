a49039356cc31164625d7e3f849e6994bb7e8b59
mometwaly <mo.metwaly94@gmail.com>
2021-08-25 16:55:16 +0200

    auth

---
+++ src/app/modules/auth/auth.component.ts
@@ -0,0 +1,53 @@
+import { AuthService, AuthResponseData } from './../../services/auth.service';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = '';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+  switchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+  onAuthenticate(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    this.isLoading = true;
+
+    let authObs: Observable<AuthResponseData>;
+    if (this.isLoginMode) {
+      const email = form.value.email;
+      const password = form.value.password;
+      authObs = this.authService.login(email, password);
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+      authObs = this.authService.signUp(email, password);
+    }
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.isLoading = false;
+        this.error = errorMessage;
+      }
+    );
+
+    form.reset();
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,135 @@
+import { User } from './../shared/user.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError, Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user = new Subject<User>();
+  token: string;
+  private tokenExpriationTimer: any;
+  constructor(private http: HttpClient) {}
+  signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-7Mog7bvk26b1KZiy1YuWVPZOGmRv9FQ',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handelError),
+        tap((resData) => {
+          this.handelAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC-7Mog7bvk26b1KZiy1YuWVPZOGmRv9FQ',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handelError),
+        tap((resData) => {
+          this.handelAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+  private handelError(errorResponse: HttpErrorResponse) {
+    let errorMessage = 'An error occured';
+    if (!errorResponse.error.error || !errorResponse.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This Email already exist';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This is Email not found';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password not correct';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+  private handelAuthentication(
+    email: string,
+    id: string,
+    token: string,
+    expireIn: number
+  ) {
+    const expiratinDate = new Date(new Date().getTime() + +expireIn * 1000);
+    const user = new User(email, id, token, expiratinDate);
+    this.user.next(user);
+    this.autoLogout(expireIn * 1000);
+    this.token = token;
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+  autoLogin() {
+    const userdata: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userdata) {
+      return;
+    }
+    const loadedUser = new User(
+      userdata.email,
+      userdata.id,
+      userdata._token,
+      new Date(userdata._tokenExpirationDate)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      //we should call auto logout
+      this.token = loadedUser.token;
+      console.log('log from autoLogin');
+    }
+  }
+  logout() {
+    if (this.tokenExpriationTimer) {
+      clearTimeout(this.tokenExpriationTimer);
+    }
+    this.tokenExpriationTimer = null;
+    this.user.next(undefined);
+    this.token = null;
+    localStorage.removeItem('userData');
+  }
+  autoLogout(expirationDuration: number) {
+    this.tokenExpriationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+}
